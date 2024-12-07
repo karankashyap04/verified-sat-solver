3f658be17f80332d6c7da0c035d2899a5bbf8099
Karan Kashyap <karankashyap@gmail.com>
2024-12-07 01:43:50 -0500

    [feat] update implementation -- no explicit formula modifications
    Rather than explicitly modifying the formula by removing satisfied clauses and removing literals from clauses, we keep the formula as is, and infer the current state of the formula/clause based on the current assignment

--- SatSolver/Dpll.lean
+++ SatSolver/Dpll.lean
@@ -4,51 +4,69 @@ import SatSolver.ReprInstances
 
 namespace SAT
 
-/- checks if a given formula is already UNSAT (if it contains
-an empty clause) -/
-def Formula.isUNSAT : Formula → Bool
-  | [] => false
-  | clause :: clauses =>
-    match clause with
-    | [] => true
-    | _ => Formula.isUNSAT clauses
+def Clause.isUNSAT (a : Assignment) : Clause → Bool :=
+  fun clause => clause.all (λ lit => Literal.eval a lit = some false)
+
+def Clause.isSAT (a : Assignment) : Clause → Bool :=
+  fun clause => clause.any (λ lit => Literal.eval a lit = some true)
+
+def Formula.isUNSAT (a : Assignment) : Formula → Bool :=
+  fun f => f.any (λ clause => clause.isUNSAT a)
+
+def Formula.isSAT (a : Assignment) : Formula → Bool :=
+  fun f => f.all (λ clause => clause.isSAT a)
 
 /- chooses an unassigned variable as the next branching variable
 in the tree search -/
-def Formula.chooseVar (f : Formula) (a : Assignment) : Option Var :=
-  let c := f.find? (λ clause => clause.any (λ lit => a lit.var = none))
-  -- c is a clause that contains an unassigned variable
-  match c with
-  | none => none
-  | some clause => -- get the literal from the clause, and return its variable
-    (clause.find? (λ lit => a lit.var = none)).bind (λ lit => some lit.var)
+def Formula.chooseVar (a : Assignment) : Formula → Option Var :=
+  fun f => f.vars.find? (λ var => a var = none)
 
-/- DPLL algorithm (for solving boolean satisfiability problem)
-returns an Assignment if a satisfying assignment exists; else
-returns none -/
+def Clause.chooseVar (a : Assignment) : Clause → Option Literal :=
+  fun clause => clause.find? (λ lit => a lit.var = none)
+
+def Formula.chooseVar2 (a : Assignment) : Formula → Option Literal :=
+  fun f =>
+    match f.find? (λ clause => clause.chooseVar a ≠ none) with
+    | some clause => clause.chooseVar a
+    | none => none
+
+
+/- DPLL implementation using a measure (to show Lean a strict subterm in
+recursive calls so it can guarantee termination) -/
 def DPLL_aux (a : Assignment) : Formula → ℕ → Option Assignment
-  | [], _ => some a -- empty formula so already SAT
-  | f, 0 => none -- safeguard to prevent infinite recursion
+  | f, 0 => none -- safeguard to prevent infinite recursion (helps show termination)
   | f, Nat.succ fuel =>
-    if f.isUNSAT then none else
-    let (a', f') := f.unitPropagate a
-    match f' with
-    | [] => some a' -- formula is now SAT
-    | _ =>
-      if f'.isUNSAT then none else
-      match f'.chooseVar a' with
-      | none => none -- we should never get here
-      | some v =>
-        let a_true := λ var => if var = v then some true else a' var
-        let tryTrue := DPLL_aux a_true (f'.simplify a_true) fuel
-        match tryTrue with
-        | some a'' => some a''
-        | none =>
-          let a_false := λ var => if var = v then some false else a' var
-          DPLL_aux a_false (f'.simplify a_false) fuel
+    if f.isSAT a then some a
+    else if f.isUNSAT a then none
+    else
+      match f.unitPropagate a with
+      | some a' => DPLL_aux a' f fuel
+      | none =>
+        match f.chooseVar2 a with -- [TODO]: maybe need to change this to chooseVar (instead of chooseVar2)
+        | none => none -- no unassigned variables (shouldn't happen)
+        | some lit =>
+          match lit with
+          -- [TODO]: might not need separate true first vs false first behavior
+          -- (had added this earlier, but with the latest implementation, this
+          -- shouldn't be necessary)
+          | Literal.pos v =>
+            let a_true := λ var => if var = v then some true else a var
+            match DPLL_aux a_true f fuel with
+            | some a' => some a'
+            | none =>
+              let a_false := λ var => if var = v then some false else a var
+              DPLL_aux a_false f fuel
+          | Literal.neg v =>
+            let a_false := λ var => if var = v then some false else a var
+            match DPLL_aux a_false f fuel with
+            | some a' => some a'
+            | none =>
+              let a_true := λ var => if var = v then some true else a var
+              DPLL_aux a_true f fuel
 
-def DPLL (a : Assignment) (f : Formula) : Option Assignment :=
-  DPLL_aux a f (f.vars.length)
+def DPLL (f : Formula) : Option Assignment :=
+  if f = [] then some (λ _ => none) else
+    DPLL_aux (λ _ => none) f f.vars.length
 
 -- def displayResult (result : Option Assignment) (f : Formula) : String :=
 --   match result with
@@ -56,13 +74,12 @@ def DPLL (a : Assignment) (f : Formula) : Option Assignment :=
 --   | some a => "SAT: " ++
 --     String.intercalate ", " (f.vars.map (λ var => s!"{repr var} := {repr (a var)}"))
 
-
--- -- EXAMPLE
+-- EXAMPLE
 -- def exampleFormula : Formula :=
 --   [[Literal.pos (Var.mk 0), Literal.neg (Var.mk 1)], [Literal.neg (Var.mk 0), Literal.pos (Var.mk 1)]]
 
 -- def satResult : Option Assignment :=
---   DPLL (λ _ => none) exampleFormula
+--   DPLL exampleFormula
 -- #eval displayResult satResult exampleFormula
 
 end SAT
--- SatSolver/UnitPropagate.lean
+++ SatSolver/UnitPropagate.lean
@@ -5,65 +5,32 @@ namespace SAT
 /- checks if a clause is a unit clause under a given assignment
 if it is, it returns the unit literal in the clause; else it
 returns none -/
-def Clause.isUnit : Clause → Option Literal
-  | [lit] => some lit
-  | _ => none
-
-/- simplifies clauses by removing literals that are false under
-the given assignment (since those literals couldn't possibly satisfy
-the clause under this assignment) -/
-def Clause.simplify (a : Assignment) : Clause → Clause
-  | [] => []
-  | lit :: lits =>
-    match Literal.eval a lit with
-    | some false => Clause.simplify a lits -- exclude lit
-    | _ => lit :: Clause.simplify a lits
-
-/- simplifies the formula by
-    1. removing all clauses that are already satisfied
-    2. simplifying all clauses -/
-def Formula.simplify (a : Assignment) : Formula → Formula
-  | [] => []
-  | clause :: clauses =>
-    let clause' := Clause.simplify a clause
-    match clause'.find? (λ lit => Literal.eval a lit = some true) with
-    | some _ => Formula.simplify a clauses -- exclude clause since it is satisfied
-    | none => clause' :: Formula.simplify a clauses
+def Clause.findUnit (a : Assignment) : Clause → Option Literal :=
+  fun clause =>
+    if clause.any (λ lit => Literal.eval a lit = some true) then none -- clause already satisfied
+    else
+      match clause.filter (λ lit => Literal.eval a lit = none) with
+      | [lit] => some lit
+      | _ => none
 
 /- returns the literal from the first unit clause it finds if it
 finds a unit clause; returns none if none of the clauses are unit
 clauses -/
-def Formula.findUnit : Formula → Option Literal
-  | [] => none
-  | clause :: clauses =>
-    match clause.isUnit with
-    | some lit => some lit
-    | none => Formula.findUnit clauses
+def Formula.findUnit (a : Assignment) : Formula → Option Literal :=
+  fun f => match f.find? (λ clause => clause.findUnit a ≠ none) with
+    | some clause => clause.findUnit a
+    | none => none
 
-/- helper function for performing unit propagation on a formula -- this
-includes an extra variable `fuel` in the recursion -- it serves no
-functional purpose beyond being a term that clearly decreases in the
-recursive calls, allowing Lean to identify that this function actually
-terminates -/
-def Formula.unitPropagateAux (a : Assignment) : Formula → ℕ → Assignment × Formula
-  | [], _ => (a, []) -- formula is empty (already SAT)
-  | f, 0 => (a, f) -- safeguard: prevent infinite recursion
-  | f, Nat.succ fuel =>
-    match f.findUnit with
-    | none => (a, f) -- no unit clause found
-    | some lit => -- unit clause found (need to propagate lit)
+/- performs unit propagation for a single variable and returns the
+updated assignment. If there is no unit clause, no unit propagation
+can be performed so none is returned -/
+def Formula.unitPropagate (a : Assignment) : Formula → Option Assignment :=
+  fun f =>
+    match f.findUnit a with
+    | none => none
+    | some lit =>
       match lit with
-      | Literal.pos v =>
-        let a' := λ var => if var = v then some true else a var
-        let f' := f.simplify a'
-        Formula.unitPropagateAux a' f' fuel
-      | Literal.neg v =>
-        let a' := λ var => if var = v then some false else a var
-        let f' := f.simplify a'
-        Formula.unitPropagateAux a' f' fuel
-
-/- perform unit propagation on a formula -/
-def Formula.unitPropagate (a : Assignment) (f : Formula) : Assignment × Formula :=
-  Formula.unitPropagateAux a f f.length
+      | Literal.pos v => some (λ var => if var = v then some true else a var)
+      | Literal.neg v => some (λ var => if var = v then some false else a var)
 
 end SAT
